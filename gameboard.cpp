c2217bffff41b605d597e0428076f9e350fb8a38
chlrod <62454601+chlrod@users.noreply.github.com>
2020-05-22 20:20:38 +0800

    Update gameboard.cpp

--- gameboard.cpp
+++ gameboard.cpp
@@ -226,7 +226,7 @@ void Game::ChessBoard::movedown(int i, int j)
 
 void Game::ChessBoard::moveup(int i, int j)
 {
-	bool combinflag;
+	bool combinflag=false;
 	if (board[i][j] == NULL)
 		return;
 	if (j == 0)
